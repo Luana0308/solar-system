3b308302394cc332d76abe7fe160966a12b83cef
Luana <luanaff-2008@hotmail.com>
2022-01-25 18:51:31 -0300

    estilizando o sistem-solar

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -5,9 +5,9 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      <div className="box-planet-card" data-testid="planet-card">
+        <img className="planImage" src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name" className="planetName">{planetName}</p>
       </div>
     );
   }
